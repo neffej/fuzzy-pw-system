4bd1a134bafdf34fe78d7a3416b8dd5a5da76237
neffej <118024182+neffej@users.noreply.github.com>
2022-12-22 16:19:14 -0500

    fixed bracket issue on js file

--- script.js
+++ script.js
@@ -106,6 +106,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword){
-  event.preventDefault();
-};
+generateBtn.addEventListener("click", writePassword)
